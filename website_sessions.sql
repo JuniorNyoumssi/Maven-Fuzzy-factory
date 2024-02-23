7fef5d93d9d0dd5fd38935ee3429f0b02986324b
Junior Alphonse <109452110+JuniorNyoumssi@users.noreply.github.com>
2024-02-22 21:00:07 -0500

    Add files via upload

--- website_sessions.sql
+++ website_sessions.sql
@@ -121,8 +121,8 @@ CREATE TEMPORARY TABLE Bounced_session
 SELECT
 	Url_landingpage.website_session_id,Url_landingpage.pageview_url,
     COUNT(DISTINCT website_pageviews.website_pageview_id) AS bounced_sessions
-FROM Url_landingpage
-LEFT JOIN website_pageviews ON Url_landingpage.website_session_id = website_pageviews.website_session_id
+FROM  Url_landingpage
+LEFT JOIN  website_pageviews ON Url_landingpage.website_session_id = website_pageviews.website_session_id
 group by Url_landingpage.website_session_id,Url_landingpage.pageview_url
 HAVING bounced_sessions = 1;
 
@@ -134,6 +134,59 @@ SELECT
 FROM  Url_landingpage
 LEFT JOIN  Bounced_session ON  Url_landingpage.website_session_id = Bounced_session.website_session_id
 GROUP BY Url_landingpage.pageview_url;
+
+
+-- TASK : Carry out a Landing page trend analysis respecting certian criteria
+
+CREATE TEMPORARY TABLE sessions_w_min_pv_id_and_view_count
+SELECT 
+     website_sessions.website_session_id,
+     MIN(website_pageviews.website_pageview_id) AS first_pageview_id,
+     COUNT(website_pageviews.website_pageview_id) AS count_pageviews
+     
+FROM  website_sessions
+    LEFT JOIN  website_pageviews
+        ON website_sessions.website_session_id = website_pageviews.website_session_id
+        
+WHERE website_sessions.created_at > '2012-06-01'
+    AND website_sessions.created_at < '2012-08-31'
+    AND website_sessions.utm_source = 'gsearch'
+    AND website_sessions.utm_campaign = 'nonbrand'
+    
+GROUP BY 
+     website_sessions.website_session_id;
+
+CREATE TEMPORARY TABLE sessions_w_counts_lander_and_created_at
+SELECT
+     sessions_w_min_pv_id_and_view_count.website_session_id,
+     sessions_w_min_pv_id_and_view_count.first_pageview_id,
+     sessions_w_min_pv_id_and_view_count.count_pageviews,
+     website_pageviews.pageview_url AS landing_page,
+     website_pageviews.created_at AS session_created_at
+     
+FROM sessions_w_min_pv_id_and_view_count
+    LEFT JOIN website_pageviews
+        ON sessions_w_min_pv_id_and_view_count.first_pageview_id = website_pageviews.website_pageview_id;
+        
+SELECT 
+   MIN(DATE(session_created_at)) AS week_start_date,
+   COUNT(DISTINCT CASE WHEN count_pageviews = 1 THEN website_session_id ELSE NULL END) AS bounced_sessions,
+   COUNT(DISTINCT CASE WHEN count_pageviews = 1 THEN website_session_id ELSE NULL END)/COUNT(DISTINCT website_session_id),
+   COUNT(DISTINCT CASE WHEN landing_page = '/home' THEN website_session_id ELSE NULL END) AS home_sessions,
+   COUNT(DISTINCT CASE WHEN landing_page = 'lander-1' THEN website_session_id ELSE NULL END) AS lamder_sessions
+
+FROM sessions_w_counts_lander_and_created_at
+
+GROUP BY
+    YEARWEEK(session_created_at)
+   
+        
+
+
+
+
+     
+
 
 
 
